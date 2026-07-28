e02018b4ab671eaa469680b3bc9d8dff718fa18a
Pramod Ghimire <pramodghimire96@gmail.com>
2026-07-28 21:20:32 +0545

    fix: home view model import issue

--- app/src/main/java/com/purrcare/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/purrcare/ui/viewmodel/HomeViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
